908c2cebf230d1184c8e2ae2ce511d8667410080
kedypvv <danila.kazakov1997@gmail.com>
2022-02-10 17:28:10 +0500

    Библиотека классов и подкл. к БД

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Library2
+{
+    public class ConnDB
+    {
+        public static MySqlConnection connection = new MySqlConnection("server = caseum.ru; port = 33333; user = st_3_8_19; password = 15676267; database = st_3_8_19");
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
